859e4f9865c4e6a1eb0ff2fb9e0f2bab673964d7
Carlos Robles <dbamastery@outlook.com>
2019-10-24 13:02:36 -0600

    Update 3_1_CreateLoginsMaskData.sql

--- 3_1_CreateLoginsMaskData.sql
+++ 3_1_CreateLoginsMaskData.sql
@@ -16,7 +16,11 @@ EXEC sp_addsrvrolemember 'dev_team', 'processadmin';
 
 -- Granting additional permissions
 GRANT VIEW SERVER STATE TO dev_team;
-GRANT EXECUTE ON DBA.dbo.sp_WhoIsActive TO dev_team;
+
+USE DBA;
+GO
+CREATE USER dev_team FOR LOGIN dev_team;
+GRANT EXECUTE ON dbo.sp_WhoIsActive TO dev_team;
 
 -- Creating user for login with read-only access in Human Resources database
 USE HumanResources;
@@ -26,6 +30,7 @@ ALTER ROLE db_datareader ADD MEMBER dev_team;
 ALTER ROLE db_datawriter ADD MEMBER dev_team;
 ALTER ROLE db_ddladmin ADD MEMBER dev_team;
 ALTER ROLE db_backupoperator ADD MEMBER dev_team;
+
 
 -- Start flag
 SELECT 'Logins succesfully created'
