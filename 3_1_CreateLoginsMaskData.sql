31fab5b7aa78bfda266bd66eaa895643bbd4c49c
Carlos Robles <dbamastery@outlook.com>
2019-10-29 18:11:11 -0600

    Update 3_1_CreateLoginsMaskData.sql

--- 3_1_CreateLoginsMaskData.sql
+++ 3_1_CreateLoginsMaskData.sql
@@ -6,34 +6,48 @@
 SET NOCOUNT ON
 
 -- Creating login for development team
-USE master;
+USE master
 GO
 
 -- Creating login
 CREATE LOGIN dev_team WITH PASSWORD=N'_D3v3L0pM3nt_',
-DEFAULT_DATABASE=HumanResources, CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF;
-EXEC sp_addsrvrolemember 'dev_team', 'processadmin';
+DEFAULT_DATABASE=HumanResources, CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
+GO
+
+EXEC sp_addsrvrolemember 'dev_team', 'processadmin'
+GO
 
 -- Granting additional permissions
-GRANT VIEW SERVER STATE TO dev_team;
+GRANT VIEW SERVER STATE TO dev_team
+GO
 
 -- Creating user login with execute permission in DBA database
-USE DBA;
+USE DBA
 GO
-CREATE USER dev_team FOR LOGIN dev_team;
-GRANT EXECUTE ON dbo.sp_WhoIsActive TO dev_team;
+
+CREATE USER dev_team FOR LOGIN dev_team
+GO
+GRANT EXECUTE ON dbo.sp_WhoIsActive TO dev_team
+GO
 
 -- Creating user for login with read-only access in Human Resources database
-USE HumanResources;
+USE HumanResources
 GO
-CREATE USER dev_team FOR LOGIN dev_team;
-ALTER ROLE db_datareader ADD MEMBER dev_team;
-ALTER ROLE db_datawriter ADD MEMBER dev_team;
-ALTER ROLE db_ddladmin ADD MEMBER dev_team;
-ALTER ROLE db_backupoperator ADD MEMBER dev_team;
+
+CREATE USER dev_team FOR LOGIN dev_team
+GO
+ALTER ROLE db_datareader ADD MEMBER dev_team
+GO
+ALTER ROLE db_datawriter ADD MEMBER dev_team
+GO
+ALTER ROLE db_ddladmin ADD MEMBER dev_team
+GO
+ALTER ROLE db_backupoperator ADD MEMBER dev_team
+GO
 
 -- Start flag
-SELECT 'Logins succesfully created';
+SELECT 'Logins succesfully created'
+GO
 
 -- ***** Masking data using Dynamic data masking ***** 
 -- ***** Employees data *****
@@ -65,6 +79,8 @@ ALTER COLUMN first_name ADD MASKED WITH (FUNCTION = 'partial(2,"XXX",2)');
 -- Last name
 ALTER TABLE HumanResources.dbo.Dependents  
 ALTER COLUMN last_name ADD MASKED WITH (FUNCTION = 'partial(2,"XXX",2)');
+GO
 
 -- End flag
 SELECT 'Data has been masked';
+GO
